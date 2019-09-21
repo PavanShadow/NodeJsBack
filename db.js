e96986fbfef90fbe551772bb65f3a800b0503956
Pavan Madusha <shadowfax9592@gmail.com>
2019-09-21 12:01:11 +0530

    sorting data

--- db.js
+++ db.js
@@ -9,18 +9,3 @@ mongoose.connect('mongodb+srv://pavanSE:pavanse@project-database-o2ixb.mongodb.n
 
 mongoose.Promise = global.Promise;
 module.exports = mongoose;
-// jjjjjjjjj
-
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://pavanSE:pavanse@project-database-o2ixb.mongodb.net/test?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// client.connect(err => {
-//   const collection = client.db("DesignProject3rd").collection("products");
-//   // perform actions on the collection object
-//   if(!err)
-//         console.log('mongodb connection succeeded..............');
-//     else
-//         console.log('Error in DB.');
-        
-//   client.close();
-// });
